4001e3f8e93b332ce31873f7e04f68c0fb025023
Emad-eldeen-Elsayed <122600696+Emad-eldeen-Elsayed@users.noreply.github.com>
2023-01-13 14:57:25 +0200

    Create myfirstpython.py
    Check balance of the cash in the company

---
+++ myfirstpython.py
@@ -0,0 +1,7 @@
+## Check balance of the cash in the company.
+actual_balance = 5
+desired_minimum_balance = 15
+if actual_balance <= desired_minimum_balance:
+    print ("We need ore cash")
+else:
+    print ("cash is good")
